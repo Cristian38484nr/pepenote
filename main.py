976f3095bd433e00467c775bd7bda71066ce58bf
cristian_price <cristianmerchan980@gmail.com>
2026-03-27 00:55:09 -0400

    V1.0

--- main.py
+++ main.py
@@ -1,1 +1,54 @@
-print("Hello word")
+import tkinter as tk
+from tkinter import messagebox
+from notes import pNotes
+import json
+
+notas = {}
+try:
+    with open('notas.json', 'r', encoding='utf-8') as f:
+        notas = json.load(f)
+except FileNotFoundError:
+    with open('notas.json', 'w', encoding='utf-8') as f:
+        json.dump({}, f)
+
+many = tk.Tk()
+many.title("Notas")
+many.geometry("800x600")
+
+titulo1 = tk.Label(many, text = "Inicio")
+titulo1.pack()
+
+text = tk.Text(many)
+text.pack()
+
+
+def vernotas(many, notas):
+    pNotes(many, notas)
+    many.withdraw()
+
+def guardaN():
+    nota = text.get("1.0", tk.END)
+    nota = nota.strip()
+    if len(nota) < 1:
+        messagebox.showerror("Error", "Debes escribir algo.")
+    else:
+        messagebox.showinfo("Exito", "Nota guardada")
+        if notas:
+            nuevo_id = max(map(int, notas.keys())) + 1
+        else:
+            nuevo_id = 1
+
+        notas[nuevo_id] = {
+            "nota": nota
+        }
+        with open('notas.json', 'w', encoding='utf-8') as f:
+            json.dump(notas, f, indent=4)
+    
+
+boton = tk.Button(many, text="Ver notas", command=lambda: vernotas(many, notas))
+boton.pack(pady=20)
+
+boton = tk.Button(many, text="Guardar nota", command=lambda: guardaN())
+boton.pack(pady=20)
+
+many.mainloop()
---
+++ notes.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+import json
+
+def pNotes(many, notas):
+    pNotes = tk.Toplevel()
+    pNotes.title("Notas guardadas")
+    pNotes.geometry("800x600")
+
+
+    titulo = tk.Label(pNotes, text = "Estas son tus notas guardadas.")
+    titulo.pack()
+
+    listbox = tk.Listbox(pNotes, selectmode=tk.MULTIPLE)  # MULTIPLE o SINGLE
+    listbox.pack(pady=10, fill=tk.BOTH, expand=True)
+
+    items = [notas]
+    for item in items:
+        listbox.insert(tk.END, item)
+
+    def delatenotes():
+        ide = id.get()
+        if ide in notas:
+            del notas[ide]
+            print(f"Borraste la nota numero {ide}")
+            pNotes.update()
+            with open('notas.json', 'w', encoding='utf-8') as f:
+                json.dump(notas, f, indent=4)
+        else:
+            print("Pvto bruto jaja")
+
+    boton = tk.Button(pNotes, text="Ver notas", command=lambda: delatenotes())
+    boton.pack(pady=20)
+
+    titulo = tk.Label(pNotes, text = "Pon el id de la nota que deseas eliminar:")
+    titulo.pack()
+
+    id = tk.Entry(pNotes)
+    id.pack()
+
+    boton = tk.Button(pNotes, text="Borrar nota", command=lambda: delatenotes())
+    boton.pack(pady=20)
